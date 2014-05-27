f11ece2d16403a0bf0cba8059f48ce653ad1c16a
Aliza Koresh <aliza-k@013.net>
2014-05-27 21:34:52 +0300

    27/5/14

--- MyHealthSpot-Code/sima/Recipes.aspx.cs
+++ MyHealthSpot-Code/sima/Recipes.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace sima
+namespace MyHealthSpot
 {
     public partial class ClsMatkonim : System.Web.UI.Page
     {
